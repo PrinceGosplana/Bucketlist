4ed756dba5159be47bee4a5a3127f3e41972c24e
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-01-02 07:06:33 +0200

    init project

---
+++ Bucketlist/Bucketlist/BucketlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketlistApp.swift
+//  Bucketlist
+//
+//  Created by OLEKSANDR ISAIEV on 02.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct BucketlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
