4f523f5a0f7525c9b38ad26a20b87b7558779d0e
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-01-03 06:23:38 +0200

    conform comparable protocol

--- Bucketlist/Bucketlist/Study/User.swift
+++ Bucketlist/Bucketlist/Study/User.swift
@@ -7,8 +7,12 @@
 
 import SwiftUI
 
-private struct User: Identifiable {
+struct User: Comparable, Identifiable {
     let id = UUID()
     var firstName: String
     var lastName: String
+    
+    static func < (lhs: User, rhs: User) -> Bool {
+        lhs.lastName < rhs.lastName
+    }
 }
