55d7c9fb2c46c282ccfd5acc2dd3a1f5d36a51e1
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-01-03 06:38:47 +0200

    writing data to the documents directory

--- Bucketlist/Bucketlist/Study/StoringDataView.swift
+++ Bucketlist/Bucketlist/Study/StoringDataView.swift
@@ -9,7 +9,17 @@ import SwiftUI
 
 struct StoringDataView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button("Read and write") {
+            let data = Data("Test message".utf8)
+            let url = URL.documentsDirectory.appending(path: "message.txt")
+            do {
+                try data.write(to: url, options: [.atomic, .completeFileProtection])
+                let input = try String(contentsOf: url)
+                print(input)
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
     }
 }
 
