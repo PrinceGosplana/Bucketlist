6642f3974c484e6e212b4cfbe3d3287fa64e6838
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-01-03 06:40:08 +0200

    view for enums

---
+++ Bucketlist/Bucketlist/Study/EnumView.swift
@@ -0,0 +1,18 @@
+//
+//  EnumView.swift
+//  Bucketlist
+//
+//  Created by OLEKSANDR ISAIEV on 03.01.2024.
+//
+
+import SwiftUI
+
+struct EnumView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EnumView()
+}
