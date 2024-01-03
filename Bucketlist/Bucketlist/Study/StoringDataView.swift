d2c13433c8e56d8f955fc31be313d3a1bf61043f
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-01-03 06:29:28 +0200

    add storing data view

---
+++ Bucketlist/Bucketlist/Study/StoringDataView.swift
@@ -0,0 +1,18 @@
+//
+//  StoringDataView.swift
+//  Bucketlist
+//
+//  Created by OLEKSANDR ISAIEV on 03.01.2024.
+//
+
+import SwiftUI
+
+struct StoringDataView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StoringDataView()
+}
