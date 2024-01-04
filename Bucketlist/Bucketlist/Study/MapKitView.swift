ebbc12c60b3a2a2929d26905ff94b9ded632a74d
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-01-04 06:21:38 +0200

    add map into a view

--- Bucketlist/Bucketlist/Study/MapKitView.swift
+++ Bucketlist/Bucketlist/Study/MapKitView.swift
@@ -5,11 +5,12 @@
 //  Created by OLEKSANDR ISAIEV on 04.01.2024.
 //
 
+import MapKit
 import SwiftUI
 
 struct MapKitView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Map()
     }
 }
 
