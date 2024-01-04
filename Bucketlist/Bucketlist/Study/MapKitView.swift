ab8a74b65151638079d8d72b3e35bfb453eaa3cf
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-01-04 06:39:17 +0200

    change location

--- Bucketlist/Bucketlist/Study/MapKitView.swift
+++ Bucketlist/Bucketlist/Study/MapKitView.swift
@@ -9,10 +9,43 @@ import MapKit
 import SwiftUI
 
 struct MapKitView: View {
-    let position = MapCameraPosition.region(MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)))
+    @State private var position = MapCameraPosition.region(
+        MKCoordinateRegion(center:
+                            CLLocationCoordinate2D(latitude: 51.507222,
+                                                   longitude: -0.1275),
+                           span: MKCoordinateSpan(latitudeDelta: 1,
+                                                  longitudeDelta: 1)))
     var body: some View {
-        Map(initialPosition: position)
-            .mapStyle(.hybrid(elevation: .realistic))
+        VStack {
+            Map(position: $position)
+                .mapStyle(.hybrid(elevation: .realistic))
+            HStack(spacing: 50) {
+                Button("Paris") {
+                    position = MapCameraPosition.region(
+                        MKCoordinateRegion(center:
+                                            CLLocationCoordinate2D(latitude: 48.8566,
+                                                                   longitude: 2.3522),
+                                           span: MKCoordinateSpan(latitudeDelta: 1,
+                                                                  longitudeDelta: 1)))
+                }
+                Button("Tokyo") {
+                    position = MapCameraPosition.region(
+                        MKCoordinateRegion(center:
+                                            CLLocationCoordinate2D(latitude: 35.6897,
+                                                                   longitude: 139.6922),
+                                           span: MKCoordinateSpan(latitudeDelta: 1,
+                                                                  longitudeDelta: 1)))
+                }
+                Button("London") {
+                    position = MapCameraPosition.region(
+                        MKCoordinateRegion(center:
+                                            CLLocationCoordinate2D(latitude: 51.507222,
+                                                                   longitude: -0.1275),
+                                           span: MKCoordinateSpan(latitudeDelta: 1,
+                                                                  longitudeDelta: 1)))
+                }
+            }
+        }
     }
 }
 
