5124bad13b1e75427b7b50cde2fb11488fd6859f
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-01-04 06:20:19 +0200

    add empty map kit view

---
+++ Bucketlist/Bucketlist/Study/MapKitView.swift
@@ -0,0 +1,18 @@
+//
+//  MapKitView.swift
+//  Bucketlist
+//
+//  Created by OLEKSANDR ISAIEV on 04.01.2024.
+//
+
+import SwiftUI
+
+struct MapKitView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapKitView()
+}
