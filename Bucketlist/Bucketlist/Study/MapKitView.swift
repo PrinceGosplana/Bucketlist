bd4bff8d245b446538673dd390f2edf225102044
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-01-04 06:48:14 +0200

    prepare new locations for our map

--- Bucketlist/Bucketlist/Study/MapKitView.swift
+++ Bucketlist/Bucketlist/Study/MapKitView.swift
@@ -8,6 +8,12 @@
 import MapKit
 import SwiftUI
 
+struct Location: Identifiable {
+    let id = UUID()
+    let name: String
+    let coordinate: CLLocationCoordinate2D
+}
+
 struct MapKitView: View {
     @State private var position = MapCameraPosition.region(
         MKCoordinateRegion(center:
@@ -15,6 +21,11 @@ struct MapKitView: View {
                                                    longitude: -0.1275),
                            span: MKCoordinateSpan(latitudeDelta: 1,
                                                   longitudeDelta: 1)))
+    
+    let locations = [
+        Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
+        Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
+    ]
     var body: some View {
         VStack {
             Map(position: $position)
