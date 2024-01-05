35754fce4b6ae72185c5249713265bf0c83b026f
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-01-05 06:32:21 +0200

    add view for face id

---
+++ Bucketlist/Bucketlist/Study/FaceIDView.swift
@@ -0,0 +1,18 @@
+//
+//  FaceIDView.swift
+//  Bucketlist
+//
+//  Created by OLEKSANDR ISAIEV on 05.01.2024.
+//
+
+import SwiftUI
+
+struct FaceIDView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FaceIDView()
+}
