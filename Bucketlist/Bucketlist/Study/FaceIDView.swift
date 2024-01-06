8c32322f56cbf20c105376a72f065420296b8af1
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-01-06 15:14:57 +0200

    add authentiation check func

--- Bucketlist/Bucketlist/Study/FaceIDView.swift
+++ Bucketlist/Bucketlist/Study/FaceIDView.swift
@@ -5,11 +5,33 @@
 //  Created by OLEKSANDR ISAIEV on 05.01.2024.
 //
 
+import LocalAuthentication
 import SwiftUI
 
 struct FaceIDView: View {
+    @State private var isUnlocked = false
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+    
+    private func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "We need to unlock your data"
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                if success {
+                    isUnlocked = true
+                } else {
+                    // there was a problem
+                }
+            }
+        } else {
+            // no biometric
+        }
     }
 }
 
