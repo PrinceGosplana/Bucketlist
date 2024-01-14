cacaf3623a88f3fc6055ed6a28ddb66a0860f149
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-01-14 06:52:48 +0200

    add new view for user location

---
+++ Bucketlist/Bucketlist/Study/LocationUserOnMapView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationUserOnMapView.swift
+//  Bucketlist
+//
+//  Created by OLEKSANDR ISAIEV on 14.01.2024.
+//
+
+import SwiftUI
+
+struct LocationUserOnMapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationUserOnMapView()
+}
