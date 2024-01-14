e88ab0aae126c19f7eb81f48cd5890a3252bd720
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-01-14 10:40:21 +0200

    update map after each tap

--- Bucketlist/Bucketlist/Study/LocationUserOnMapView.swift
+++ Bucketlist/Bucketlist/Study/LocationUserOnMapView.swift
@@ -11,25 +11,33 @@ import SwiftUI
 struct LocationUserOnMapView: View {
     let startPosition = MapCameraPosition.region(
         MKCoordinateRegion(
-        center: CLLocationCoordinate2D(latitude: 56, longitude: -3),
-        span: MKCoordinateSpan(latitudeDelta: 10, longitudeDelta: 10)
+            center: CLLocationCoordinate2D(latitude: 56, longitude: -3),
+            span: MKCoordinateSpan(latitudeDelta: 10, longitudeDelta: 10)
         )
     )
     @State private var locations = [Location]()
     
     var body: some View {
         MapReader { proxy in
-            Map(initialPosition: startPosition)
-                .onTapGesture { position in
-                    if let coordinate = proxy.convert(position, from: .local) {
-                        let newLocation = Location(id: UUID(),
-                                                   name: "New Location",
-                                                   description: "",
-                                                   latitude: coordinate.latitude,
-                                                   longitude: coordinate.longitude)
-                        locations.append(newLocation)
-                    }
+            Map(initialPosition: startPosition) {
+                ForEach(locations) { location in
+                    Marker(location.name,
+                           coordinate:
+                            CLLocationCoordinate2D(latitude: location.latitude,
+                                                   longitude: location.longitude)
+                    )
                 }
+            }
+            .onTapGesture { position in
+                if let coordinate = proxy.convert(position, from: .local) {
+                    let newLocation = Location(id: UUID(),
+                                               name: "New Location",
+                                               description: "",
+                                               latitude: coordinate.latitude,
+                                               longitude: coordinate.longitude)
+                    locations.append(newLocation)
+                }
+            }
         }
     }
 }
