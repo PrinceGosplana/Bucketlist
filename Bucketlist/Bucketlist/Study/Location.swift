98d48adb2aebe6a65518fe730de9274b2a0585e3
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-01-15 06:29:06 +0200

    add example for custom location struct and make it only for debug

--- Bucketlist/Bucketlist/Study/Location.swift
+++ Bucketlist/Bucketlist/Study/Location.swift
@@ -19,4 +19,11 @@ struct Location: Codable, Equatable, Identifiable {
         CLLocationCoordinate2D(latitude: latitude,
                                longitude: longitude)
     }
+    #if DEBUG
+    static let example = Location(id: UUID(),
+                                  name: "Buckingham Palace",
+                                  description: "Lit by over 40.000 lightbulbs",
+                                  latitude: 51.501,
+                                  longitude: -0.141)
+    #endif
 }
