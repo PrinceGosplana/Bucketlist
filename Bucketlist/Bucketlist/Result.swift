2ba3b7754f4ad39ccbd4c475c8fc7bf21b9e49be
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-01-21 07:35:41 +0200

    add several structs for download elements

--- Bucketlist/Bucketlist/Result.swift
+++ Bucketlist/Bucketlist/Result.swift
@@ -5,4 +5,16 @@
 //  Created by OLEKSANDR ISAIEV on 21.01.2024.
 //
 
-import Foundation
+struct Result: Codable {
+    let query: Query
+}
+
+struct Query: Codable {
+    let pages: [Int: Page]
+}
+
+struct Page: Codable {
+    let pageId: Int
+    let title: String
+    let terms: [String: [String]]?
+}
