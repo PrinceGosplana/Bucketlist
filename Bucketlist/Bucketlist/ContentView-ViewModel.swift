8a9260c35cc9bb44eea36e30fc18825bc7ba8971
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-01-31 08:01:32 +0200

    add empty case if we do not have biometrics

--- Bucketlist/Bucketlist/ContentView-ViewModel.swift
+++ Bucketlist/Bucketlist/ContentView-ViewModel.swift
@@ -69,6 +69,8 @@ extension LocationUserOnMapView {
                         // error
                     }
                 }
+            } else {
+                // no biometrics
             }
         }
     }
